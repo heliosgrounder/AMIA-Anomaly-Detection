b9857f66321b79fa138526ad63d95eb11226f085
heliosgrounder <yuldoshev.amir@gmail.com>
2024-12-14 14:49:22 +0500

    intermediate dataset. found that we can have multiple results by rad_id in train.csv

--- src/nn/AMIADataset.py
+++ src/nn/AMIADataset.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 from PIL import Image
 
+import torch
 from torch.utils.data import Dataset
+from torchvision.tv_tensors import BoundingBoxes
 
 
 class AMIADataset(Dataset):
@@ -20,12 +22,33 @@ class AMIADataset(Dataset):
         self.df_annotations = pd.read_csv(os.path.join(data_folder, "train.csv"))
         self.df_annotations.set_index(__KEY, inplace=True)
 
+
         self.images_path = []
-        for image_path in os.listdir(os.path.join(data_folder, "train", "train")):
-            self.images_path.append(os.path.join(data_folder, "train", "train", image_path))
+        for image_filename in os.listdir(os.path.join(data_folder, "train", "train")):
+            self.images_path.append(os.path.join(data_folder, "train", "train", image_filename))
 
     def __len__(self):
         return len(self.images_path)
 
     def __getitem__(self, idx):
-        pass
+        image_path = self.images_path[idx]
+        image_uuid = os.path.splitext(os.path.basename(image_path))[0]
+        img_size = self.df_img_size.loc[image_uuid].to_list()
+        annotation = self.df_annotations.loc[image_uuid].to_list()
+
+        image = Image.open(image_path).convert("RGB")
+        label = torch.tensor(annotation[1], dtype=torch.int8)
+        bbox = []
+        if label != 14:
+            bbox = annotation[3:]
+        bbox = BoundingBoxes([bbox], format="xyxy", canvas_size=torch.Size(img_size))
+
+        # transform part (incomplete)
+
+
+        target = {
+            "label": label,
+            "bbox": bbox
+        }
+
+        return image, target
