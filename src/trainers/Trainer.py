a30d4615296e77c3f4aaece6deb3194a2f0f2fb3
heliosgrounder <yuldoshev.amir@gmail.com>
2025-01-31 16:09:41 +0500

    create train_model, test_model and fit functions

--- src/trainers/Trainer.py
+++ src/trainers/Trainer.py
@@ -7,10 +7,11 @@ from tqdm import tqdm
 from src.nn.models.FasterRCNN import FasterRCNN
 from src.nn.datasets.AMIADataset import AMIADataset, get_transform
 from src.utils.constants import DEVICE
+from src.utils.utils import collate_fn
 
 class Trainer:
     def __init__(self, 
-                 batch_size=16, 
+                 batch_size=8, 
                  learning_rate=5e-3,
                  device=DEVICE, 
                  examination=False):
@@ -22,13 +23,12 @@ class Trainer:
         self.batch_size=batch_size
         self.learning_rate = learning_rate
         
-        self.train_dataset = AMIADataset(
+        self.dataset = AMIADataset(
             transform=get_transform(),
         )
-        self.test_dataset = AMIADataset(
-            transform=get_transform(),
-            train=False
-        )
+        train_size = int(0.8 * len(self.dataset))
+        test_size = len(self.dataset) - train_size
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.dataset, [train_size, test_size])
 
         if examination:
             self.train_dataset = Subset(self.train_dataset, range(50))
@@ -38,27 +38,58 @@ class Trainer:
         self.train_loader = DataLoader(
             self.train_dataset, 
             batch_size=self.batch_size,
-            shuffle=True
+            shuffle=True,
+            collate_fn=collate_fn
         )
         self.test_loader = DataLoader(
-            self.test_loader,
+            self.test_dataset,
             batch_size=self.batch_size,
-            shuffle=False
+            shuffle=False,
+            collate_fn=collate_fn
         )
 
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.learning_rate, momentum=0.9, weight_decay=5e-4)
 
-
-
-
     def train_model(self):
         self.model.train()
 
+        running_loss = 0.0
+        for images, targets in tqdm(self.train_loader, desc="Training"):
+            images = list(img.to(self.device) for img in images)
+            targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
+
+            loss_dict = self.model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+
+            self.optimizer.zero_grad()
+            losses.backward()
+            self.optimizer.step()
+
+            running_loss += losses.item()
+
+        avg_loss = running_loss / len(self.train_loader)
+        return avg_loss
+
+    @torch.no_grad()
     def test_model(self):
-        self.model.eval()
+        self.model.train()
+
+        running_loss = 0.0
+        for images, targets in tqdm(self.test_loader, desc="Testing"):
+            images = list(img.to(self.device) for img in images)
+            targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
+
+            loss_dict = self.model(images, targets)
+            losses = sum(loss for loss in loss_dict.values())
+
+            running_loss += losses.item()
+        
+        avg_loss = running_loss / len(self.test_loader)
+        return avg_loss
 
     def fit(self, num_epochs=10):
         for epoch in range(num_epochs):
             train_loss = self.train_model()
             test_loss = self.test_model()
+            print(f"EPOCH: {epoch + 1}/{num_epochs} TRAIN LOSS: {train_loss} TEST LOSS: {test_loss}")
 
