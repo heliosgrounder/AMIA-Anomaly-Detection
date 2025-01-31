90c0a00dfb374755246c4ad6fc929d1969cdc401
heliosgrounder <yuldoshev.amir@gmail.com>
2025-01-31 12:36:38 +0500

    update Dataset, remove label_map

--- src/nn/AMIADataset.py
+++ src/nn/AMIADataset.py
@@ -10,23 +10,6 @@ from torchvision.tv_tensors import BoundingBoxes
 
 class AMIADataset(Dataset):
     def __init__(self, data_folder="data", transform=None, train=True):
-        self.label_map = {
-            0: "Aortic enlargement",
-            1: "Atelectasis",
-            2: "Calcification",
-            3: "Cardiomegaly",
-            4: "Consolidation",
-            5: "ILD",
-            6: "Infiltration",
-            7: "Lung Opacity",
-            8: "Nodule/Mass",
-            9: "Other lesion",
-            10: "Pleural effusion",
-            11: "Pleural thickening",
-            12: "Pneumothorax",
-            13: "Pulmonary fibrosis",
-            14: "No finding"
-        }
         __KEY = "image_id"
         self.data_folder = data_folder
         self.transform = transform
