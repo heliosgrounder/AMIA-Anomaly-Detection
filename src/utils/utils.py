e6679a15975fe8d991d1f841a00017b235b5c769
heliosgrounder <yuldoshev.amir@gmail.com>
2025-02-08 12:18:59 +0500

    iou function in utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,2 +1,20 @@
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+
+def iou(bbox1, bbox2):
+    x1 = max(bbox1[0], bbox2[0])
+    y1 = max(bbox1[1], bbox2[1])
+    x2 = min(bbox1[2], bbox2[2])
+    y2 = min(bbox1[3], bbox2[3])
+
+    intersection_width = max(x2 - x1, 0)
+    intersection_height = max(y2 - y1, 0)
+    intersection_area = intersection_width * intersection_height
+
+    bbox1_area = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    bbox2_area = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+
+    union_area = bbox1_area + bbox2_area - intersection_area
+
+    return intersection_area / union_area if union_area else 0
