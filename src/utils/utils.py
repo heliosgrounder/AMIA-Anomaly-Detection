61ec394cc000e8878ef0ec8126aaea8f7456d782
heliosgrounder <yuldoshev.amir@gmail.com>
2025-02-08 13:07:06 +0500

    adding merging bboxes for iou bboxes different radiologists

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def collate_fn(batch):
     return tuple(zip(*batch))
 
@@ -18,3 +21,33 @@ def iou(bbox1, bbox2):
     union_area = bbox1_area + bbox2_area - intersection_area
 
     return intersection_area / union_area if union_area else 0
+
+
+def merge_bbox(df_group, iou_threshold=0.5):
+    bboxes = df_group[["x_min", "y_min", "x_max", "y_max"]].values
+    closed = np.zeros(len(bboxes), dtype=bool)
+    bbox_groups = []
+
+    for i in range(len(bboxes)):
+        if closed[i]:
+            continue
+            
+        bbox_group = [i]
+        closed[i] = True
+
+        for j in range(i + 1, len(bboxes)):
+            if closed[j]:
+                continue
+                
+            if any(iou(bboxes[k], bboxes[j]) > iou_threshold for k in bbox_group):
+                bbox_group.append(j)
+                closed[j] = True
+        bbox_groups.append(bbox_group)
+
+    merged_bboxes = []
+    for group in bbox_groups:
+        bboxes_group = bboxes[group]
+        mean_bbox = bboxes_group.mean(axis=0)
+        merged_bboxes.append(mean_bbox)
+    
+    return merged_bboxes
