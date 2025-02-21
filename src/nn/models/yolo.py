eb089e01d1c85f3b76cda7795a34fa68e37e51fd
heliosgrounder <yuldoshev.amir@gmail.com>
2025-02-21 10:52:01 +0500

    init for yolov1

--- src/nn/models/yolo.py
+++ src/nn/models/yolo.py
@@ -2,5 +2,9 @@ import torch
 import torch.nn as nn
 
 class YOLOv1(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, num_classes=15, grid_size=7, bb=2):
+        super(YOLOv1, self).__init__()
+
+        self.grid_size = grid_size
+        self.bb = bb
+        self.num_clusses = num_classes
