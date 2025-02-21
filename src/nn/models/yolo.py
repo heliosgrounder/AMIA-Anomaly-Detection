4f82e8145bec2d9f505267c6bdd8e37dd8c3be25
heliosgrounder <yuldoshev.amir@gmail.com>
2025-02-21 10:59:55 +0500

    complete yolov1 implementation

--- src/nn/models/yolo.py
+++ src/nn/models/yolo.py
@@ -2,9 +2,54 @@ import torch
 import torch.nn as nn
 
 class YOLOv1(nn.Module):
-    def __init__(self, num_classes=15, grid_size=7, bb=2):
+    def __init__(self, num_classes=14, grid_size=7, bb=2):
         super(YOLOv1, self).__init__()
 
         self.grid_size = grid_size
         self.bb = bb
-        self.num_clusses = num_classes
+        self.num_classes = num_classes
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(192, 128, kernel_size=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 256, kernel_size=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(0,1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.1)
+        )
+
+        self.fully_connected = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024 * 7 * 7, 4096),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+            nn.Linear(4096, self.grid_size * self.grid_size * (self.bb * 5 + self.num_classes))
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.fully_connected(x)
+
+        return x.view(-1, self.grid_size, self.grid_size, self.bb * 5 + self.num_classes)
